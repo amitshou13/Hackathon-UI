9f25444903ac3ee55f7100768d00a084c6009a46
Rahul Koduri <rahulkoduri96@gmail.com>
2024-08-29 02:11:19 +0530

    Update

--- script.js
+++
@@ -1,5 +0,0 @@
-hero = document.querySelector(".hero");
-whyUs = document.querySelector(".why-us");
-howItWorks = document.querySelector(".how-it-works");
-
-about = document.querySelector(".about");
